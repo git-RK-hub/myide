c266c63720a9527a3f36f3826d94dfb01e676526
Rishi Kumar <rishi3pb2@gmail.com>
2021-09-14 12:19:23 +0530

    express server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const { cPlusPlus, cOnly } = require("./compiler");
-cOnly("hello.c");
-cPlusPlus("hello.cpp");
+const express = require("express");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const app = express();
+require("dotenv").config();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const port = 4044;
+
+app.listen(port, () => {
+  console.log(chalk.bold.redBright("Greetings from port " + port));
+});
